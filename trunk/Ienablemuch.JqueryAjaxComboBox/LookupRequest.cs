c82e7bedfd5fed8cfe353943854c8f4721f704c2
i.enable.much@gmail.com <i.enable.much@gmail.com@e08c60ef-0d8d-a88b-6e7d-f8e81094d603>
2011-10-23 07:31:05 +0000

    Initial Code Sync

---
+++ trunk/Ienablemuch.JqueryAjaxComboBox/LookupRequest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+namespace Ienablemuch.JqueryAjaxComboBox
+{
+    public class LookupRequest
+    {
+        [DisplayFormat(ConvertEmptyStringToNull = false)]
+        public string q_word { get; set; }
+
+        [DisplayFormat(ConvertEmptyStringToNull = false)]
+        public string primary_key { get; set; }
+        public int per_page { get; set; }
+        public int page_num { get; set; }
+
+        // To facilitate cascading, I modified Jquery Ajax ComboBox and added this property, this is the property being sent by the client
+        [DisplayFormat(ConvertEmptyStringToNull = false)]
+        public string cascaded_word { get; set; }
+    }
+}
